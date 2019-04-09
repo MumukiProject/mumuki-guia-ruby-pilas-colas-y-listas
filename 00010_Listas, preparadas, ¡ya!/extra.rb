06be8cf88536998d9b2838a3635e40f5eea3003d
felipe@mumuki.org <felipe@mumuki.org>
2019-04-09 18:31:29 +0000

    Mumuki Export on 2019-04-09 18:31:29 +0000

--- 00010_Listas, preparadas, ¡ya!/extra.rb
+++ 00010_Listas, preparadas, ¡ya!/extra.rb
@@ -1,1 +1,13 @@
+class Persona
+end
+
+rocio = Persona.new
+ivana = Persona.new
+daniela = Persona.new
+laura = Persona.new
+carolina = Persona.new
+monica = Persona.new
+karen = Persona.new
+nadia = Persona.new
+
 posiciones_carrera = [rocio, ivana, daniela, laura, carolina, monica, karen, nadia]
