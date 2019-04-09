910db328b15969dfdf620906f0586ca04b48983b
felipe@mumuki.org <felipe@mumuki.org>
2019-04-09 18:37:12 +0000

    Mumuki Export on 2019-04-09 18:37:12 +0000

--- 00011_Lista el pollo/extra.rb
+++ 00011_Lista el pollo/extra.rb
@@ -1,9 +1,16 @@
 class Comida
+  def initialize(nombre)
+    @nombre = nombre
+  end
+  
+  def nombre
+    @nombre
+  end
 end
 
-vitel_tone = Comida.new
-almendrado = Comida.new
-pollo_con_pure = Comida.new
-cafe = Comida.new
+vitel_tone = Comida.new("Vitel toné")
+almendrado = Comida.new("Almendrado")
+pollo_con_pure = Comida.new("Pollo con puré")
+cafe = Comida.new("Café")
 
 menu_ejecutivo = [vitel_tone, almendrado, cafe]
