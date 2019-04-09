102e523fe449b01c4f4bc0eee126677620b57bcd
felipe@mumuki.org <felipe@mumuki.org>
2019-04-09 14:31:32 +0000

    Mumuki Export on 2019-04-09 14:31:32 +0000

--- 00003_Push Pop/extra.rb
+++ 00003_Push Pop/extra.rb
@@ -1,1 +1,11 @@
-lugares_de_mi_barrio = [plaza, museo, escuela, hospital, estacionDeTren]
+class Lugar
+end
+
+plaza = Lugar.new
+museo = Lugar.new
+escuela = Lugar.new
+hospital = Lugar.new
+estacion_de_tren = Lugar.new
+kiosco = Lugar.new
+
+lugares_de_mi_barrio = [plaza, museo, escuela, hospital, estacion_de_tren]
